d406a00234f7d09225cc1b97db21a3c4ae0e0cdb
David Domínguez Blanco <60604817+DavidDominguez2@users.noreply.github.com>
2023-03-22 22:46:56 +0100

    Responsive auth forms y validation

--- VTChallenge/Models/Users.cs
+++ VTChallenge/Models/Users.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
 
@@ -9,19 +12,37 @@ namespace VTChallenge.Models {
         [Column("UID")]
         public string Uid { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Complete el campo")]
+        [StringLength(255)]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Por favor ingrese un email válido")]
+        //COMPROBAR SI EXISTE YA EL EMAIL
+        //[Remote("CheckEmailAvailability", "Account", HttpMethod = "POST", ErrorMessage = "Este Tag ya está en uso")]
         [Column("EMAIL")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Complete el campo")]
+        [StringLength(255)]
+        [DataType(DataType.Password)]
+        [PasswordPropertyText]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$",
+        ErrorMessage = "La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula, un número y un caracter especial")]
         [Column("PASSWORD")]
         public string Password { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Complete el campo")]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        [NotMapped]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Complete el campo")]
+        [StringLength(255)]
         [Column("NAME")]
         public string Name { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Complete el campo")]
+        [StringLength(5, MinimumLength = 3, ErrorMessage = "El Tag debe tener entre 3 y 5 caracteres")]
         [Column("TAG")]
         public string Tag { get; set; }
 
